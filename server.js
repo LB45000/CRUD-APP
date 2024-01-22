5253df7532d4889e85f3b514cde73fdc3cfa5c12
LB45000 <144392042+LB45000@users.noreply.github.com>
2024-01-22 14:36:35 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const uri = "mongodb+srv://l7476767:YYBFsG2THKVFpRzc@cluster0.ehrrzpd.mongodb.net/";
+
+MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database');
+    const db = client.db('test');
+    const usersCollection = db.collection('users');
+
+    app.get('/', (req, res) => {
+      usersCollection.find().toArray()
+        .then(results => res.render('index.ejs', { users: results }))
+        .catch(error => console.error(error));
+    });
+
+    app.post('/users', (req, res) => {
+      usersCollection.insertOne(req.body)
+        .then(result => res.redirect('/'))
+        .catch(error => console.error(error));
+    });
+
+    app.put('/users', (req, res) => {
+      usersCollection.findOneAndUpdate(
+        { username: req.body.username },
+        { $set: { password: req.body.newPassword } },
+        { upsert: false }
+      )
+      .then(result => res.json('Success'))
+      .catch(error => console.error(error));
+    });
+
+    app.delete('/users', (req, res) => {
+      usersCollection.deleteOne(
+        { username: req.body.username }
+      )
+      .then(result => res.json('Deleted user'))
+      .catch(error => console.error(error));
+    });
+  })
+  .catch(error => console.error(error));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
